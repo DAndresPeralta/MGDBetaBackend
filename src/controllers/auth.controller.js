7e016fe3907c20cde34af475134faa17c41921b9
dandresperalta <danteandresperalta@gmail.com>
2025-01-16 14:00:51 -0300

    V 1.0.0

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const loginUserController = (req, res, next) => {
         secure: true,
         maxAge: 600000,
         sameSite: "none", //VER para LOCAL - usar "lax" o borrar atributo
-        domain: "https://mgd-beta-front.vercel.app", //Borrar para local
+
         //   path: "/", // This line is to confirm
       });
 

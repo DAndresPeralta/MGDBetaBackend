d114c784855ee5632524fa09eb8174f69fb31031
dandresperalta <danteandresperalta@gmail.com>
2025-01-18 14:53:50 -0300

    V 1.0.2

--- src/middlewares/limiter.middleware.js
+++ src/middlewares/limiter.middleware.js
@@ -2,6 +2,9 @@ import rateLimiter from "express-rate-limit";
 
 export const loginLimiter = rateLimiter({
   windowMs: 5 * 60 * 1000,
-  max: 5,
+  max: 10,
   message: "Too many requests",
+  standardHeaders: true,
+  legacyHeaders: false,
+  trustProxy: true,
 });
